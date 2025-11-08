a09d7a4bde8b8117445c225ecfc4989c2faaef1a
BM-ACADEMY <teamkamarweb@gmail.com>
2025-11-08 15:43:02 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,18 +3,24 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Aboutsection from "./components/About";
 
-
-import Home from "./pages/Home";
-import JoinSection from "./pages/Join"
+import Joinsection from "./Pages/Join";
+import Home from "./Pages/Home";
+import JoinForm from "./Pages/JoinForm";
+import PledgeCard from "./Pages/Pledge";
+import { Scrolltotop } from "./ScrollToTop";
 
 export default function App() {
   return (
     <Router>
       <Navbar />
+      <Scrolltotop/>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/about" element={<Aboutsection />} />
-        <Route path="/join" element={<JoinSection />} />
+        {/* <Route path="/join" element={<Joinsection />} /> */}
+        <Route path="/join-form" element={<JoinForm/>}/>
+        <Route path="/PledgeCard" element={<PledgeCard/>}/>
+        <Route path="/join-us" element={<JoinForm />} />
       </Routes>
     </Router>
   );
