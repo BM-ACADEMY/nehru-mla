b038c0658a7771e1c3261bf8e72e47b133335c04
BM-ACADEMY <teamkamarweb@gmail.com>
2025-12-08 11:06:21 +0530

    image fix

--- server/nehru_backend/urls.py
+++ server/nehru_backend/urls.py
@@ -20,4 +20,4 @@ path('api/complaints/', include('nehru_backend.complaints.urls')),
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
